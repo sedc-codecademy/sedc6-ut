693760de1ebe81779a35496ba14b096237d930a6
Wekoslav Stefanovski <swekster@gmail.com>
2018-08-24 19:43:09 +0200

    added numberconverter unit test

---
+++ CSharpUnitTesting/CalculatorTests/NumberConverterTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorLib;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class NumberConverterTests
+    {
+        private NumberConverter converter;
+
+        [TestInitialize]
+        public void Init()
+        {
+            converter = new NumberConverter();
+        }
+
+        [TestMethod]
+        public void The_Number_0_should_return_zero()
+        {
+            // 1. Arrange
+            var input = 0;
+            var expected = "zero";
+
+            // 2. Act
+            var actual = converter.ToWords(input);
+
+            // 3. Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
